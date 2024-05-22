6da10e3f68b79a968d41ac6205c1f56868b896be
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2024-05-22 14:10:02 +0800

    initial commit

---
+++ components/Greetings.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { ThemedText } from "@/components/ThemedText"
+
+const Greetings = () => {
+    return (
+        <ThemedText>Greetings!! From the component</ThemedText>
+    )
+}
+
+export default Greetings;
--- test.js
+++ test.js
@@ -3,20 +3,16 @@ const person = {
   position: "Acumen Trainer",
   yrsOfExperience: 5,
   skills: ["NodeJS", "PHP", "Python"],
+  greet: () => {
+    console.log(person);
+  },
+  goodBye: function () {
+    console.log(this.name);
+  },
 };
 
 // Anthony Lloveras skills: NodeJS
 // Anthony Lloveras skills: PHP
 //Anthony Lloveras skills: Python
 
-const personSkills = person.skills.map(function (newskill) {
-  return `Anthony Lloveras skills: ${newskill}`;
-});
-
-console.log(personSkills);
-
-// const personSkills = person.skills.map(
-//   (skill) => `${person.name} skills: ${skill}`
-// );
-// const opInterpolate = personSkills.join("\n");
-// console.log(opInterpolate);
+person.greet();
