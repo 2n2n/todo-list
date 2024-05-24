35fb00a206a7e8a80636b757ababcf47d4a03dd5
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2024-05-24 13:13:50 +0800

    added prebuild files, and fix issue with unchecking the todo item

--- app/(tabs)/completed.tsx
+++ app/(tabs)/completed.tsx
@@ -1,6 +1,5 @@
 import CheckBoxItem from '@/components/CheckBoxItem';
 import TodoContext from '@/context/todo.context';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { useContext } from 'react';
 import { ScrollView, Text, View } from 'react-native';
 
@@ -23,7 +22,7 @@ export default function CompletedList() {
         { tasks
           .filter((_task) => _task.status === 'completed')
           .map((task) => {
-          return <CheckBoxItem key={task.id} task={task} onUnCheck={() => onUnCheck(task.id)}/>
+          return <CheckBoxItem key={task.id} task={task} onCheck={() => onUnCheck(task.id)}/>
         })}
       </ScrollView>
     </View>
--- components/CheckBoxItem.tsx
+++ components/CheckBoxItem.tsx
@@ -1,21 +1,12 @@
 import { View, Text } from "react-native";
 import Checkbox from 'expo-checkbox';
-import { useState, useEffect } from "react";
 
 
-const CheckBoxItem = ({ task, onCheck, onUnCheck }) => {
-    const [isChecked, setIsChecked] = useState<boolean>(task.status === 'completed');
-
-    useEffect(() => {
-        if(isChecked === true) {
-            onCheck();
-        }
-    }, [isChecked])
-
+const CheckBoxItem = ({ task, onCheck }) => {
     return (
         <View style={{ flexDirection: 'row', marginVertical: 10 }}>
-            <Checkbox value={isChecked} onValueChange={setIsChecked} />
-            <Text style={{ marginLeft: 10, textDecorationLine: isChecked? 'line-through': 'none' }}>{task.value}</Text>
+            <Checkbox value={task.status ==='completed'} onValueChange={onCheck} />
+            <Text style={{ marginLeft: 10, textDecorationLine: task.status === 'completed' ? 'line-through': 'none' }}>{task.value}</Text>
         </View>
     )
 }
